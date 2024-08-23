9e25422dab4178b85a8919e62992c5ce389e6c6c
ItKarma <loppesofc@gmail.com>
2024-08-23 07:04:48 -0300

    fix

--- src/utils/balanChange.js
+++ src/utils/balanChange.js
@@ -3,7 +3,7 @@ import userRepo from "../repository/userRepo.js";
 
 export default async function changebalance(user, valor) {
     if(user.balance == 0){
-        return
+        return 0
     }
     let newBalance = user.balance - valor
     await userRepo.changebalance(user.idUser, newBalance);
